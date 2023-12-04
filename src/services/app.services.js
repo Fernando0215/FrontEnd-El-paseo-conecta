20fb8c0ea8a1403a1c9a2f496277f44929eec704
Fernando0215 <00103722@uca.edu.sv>
2023-12-03 23:38:12 -0600

    coneccion deployada

--- src/services/app.services.js
+++ src/services/app.services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3002/api/v1";
+const BASE_URL = "https://red-lucky-cuttlefish.cyclic.app/api/v1/";
 
 export const createProducto = async (formData) => {
     try {
